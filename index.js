6c708a0fe4736f1feccc4d6ecb2596a80674c873
mesaaransh <mesaaransh@gmail.com>
2025-05-07 07:20:57 +0530

    new codes added

--- index.js
+++ index.js
@@ -6,15 +6,8 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
-
-
-app.get("/1", function(req, res){
-    const fileContent = fs.readFileSync('./codes/1.txt', 'utf8');
-    res.send(fileContent)
-})
-
-app.get("/2", function(req, res){
-    const fileContent = fs.readFileSync('./codes/2.txt', 'utf8');
+app.get("/:num", function(req, res){
+    const fileContent = fs.readFileSync(`./codes/${req.params.num}.txt`, 'utf8');
     res.send(fileContent)
 })
 
